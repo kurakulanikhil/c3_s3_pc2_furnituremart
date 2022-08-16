8df668465c789d90538a7e6bace946ab23d7554f
Kurakula nikhil <nikhildhoni17@gmail.com>
2022-08-16 22:46:19 +0530

    solution is done

--- src/com/furniture/FurnitureItem.java
+++ src/com/furniture/FurnitureItem.java
@@ -1,2 +1,78 @@
-package com.furniture;public class FurnitureItem {
+package com.furniture;
+
+public class FurnitureItem {
+    private int furnitureCode;
+    private String furnitureType;
+    private String gradeOfFurniture;
+    private String color;
+    private String furnitureUsage;
+    private double price;
+    private static final double DISCOUNT = 5;
+
+    public int getFurnitureCode() {
+        return furnitureCode;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setFurnitureCode(int furnitureCode) {
+        this.furnitureCode = furnitureCode;
+    }
+
+    public String getFurnitureType() {
+        return furnitureType;
+    }
+
+    public void setFurnitureType(String furnitureType) {
+        this.furnitureType = furnitureType;
+    }
+
+    public String getGradeOfFurniture() {
+        return gradeOfFurniture;
+    }
+
+    public void setGradeOfFurniture(String gradeOfFurniture) {
+        this.gradeOfFurniture = gradeOfFurniture;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFurnitureUsage() {
+        return furnitureUsage;
+    }
+
+    public void setFurnitureUsage(String furnitureUsage) {
+        this.furnitureUsage = furnitureUsage;
+    }
+
+    public FurnitureItem() {
+        furnitureCode =0;
+        furnitureType ="";
+        gradeOfFurniture = "";
+        color = "";
+        furnitureUsage = "";
+        price = 0.0;
+
+    }
+    public double calculateDiscount(){
+        double discountPrice = 0;
+        if(getGradeOfFurniture().equals("grade1") || getFurnitureUsage().equals("outdoor"));
+        discountPrice =  (DISCOUNT * price)/100;
+        return price - discountPrice;
+
+    }
+
+
 }
--- src/com/furniture/furnitureImpl.java
+++ src/com/furniture/furnitureImpl.java
@@ -1,2 +1,20 @@
-package com.furniture;public class furnitureImpl {
+package com.furniture;
+
+public class furnitureImpl {
+    public static void main(String[] args){
+        FurnitureItem object = new FurnitureItem();
+        object.setFurnitureCode(12345);
+        object.setColor("green");
+        object.setFurnitureType("chair");
+        object.setGradeOfFurniture("grade1");
+        object.setFurnitureUsage("outdoor");
+        object.setPrice(5000);
+        System.out.println(object.getColor());
+        System.out.println(object.getFurnitureType());
+        System.out.println(object.getFurnitureCode());
+        System.out.println(object.getFurnitureUsage());
+        System.out.println(object.getGradeOfFurniture());
+        System.out.println(object.getPrice());
+        System.out.println(object.calculateDiscount());
+    }
 }
